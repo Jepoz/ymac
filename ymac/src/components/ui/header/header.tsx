7ad034f911127591e75821dd07a1be034c078131
Jepos <110927645+Jepos@users.noreply.github.com>
2025-07-05 00:55:53 -0700

    header menu smooth fixed

--- ymac/src/components/ui/header/header.tsx
+++ ymac/src/components/ui/header/header.tsx
@@ -1,9 +1,8 @@
 import { useState, useContext  } from 'react';
-import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 import { FiMenu, FiX } from 'react-icons/fi';
 import styles from './header.module.scss';
 import { ScrollContext } from '../../../context/scroll-context';
-
 
 const Header = () => {  
   const scroll = useContext(ScrollContext);
@@ -39,22 +38,22 @@ const Header = () => {
 
         <nav className={`${styles.nav} ${menuOpen ? styles.active : ''}`}>
           <ul className={styles.navList}>
-            <li className={activeSection === 'home' ? styles.active : ''}
+            <li className={classNames({ [styles.active]: activeSection === 'home' })}
                 onClick={() => scrollToSection(homeSectionRef)}>              
               Home              
             </li>
-            <li className={activeSection === 'about' ? styles.active : ''}
+            <li className={classNames({ [styles.active]: activeSection === 'about' })}
                 onClick={() => scrollToSection(aboutUsSectionRef)}>              
                Sobre nosotros              
             </li>
             <li>              
               <img className={styles.brand} src='images/brand/brand-simple.png'/>
             </li>
-            <li className={activeSection === 'products' ? styles.active : ''}
+            <li className={classNames({ [styles.active]: activeSection === 'products' })}
                 onClick={() => scrollToSection(productsSectionRef)}>              
               Productos              
             </li>
-            <li className={activeSection === 'contact' ? styles.active : ''}
+            <li className={classNames({ [styles.active]: activeSection === 'contact' })}
                 onClick={() => scrollToSection(contactSectionRef)}>              
               Contacto              
             </li>            
