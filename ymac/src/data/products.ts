11fa6397e3f09e7181705b91f57c4b74a58ac081
Jepos <110927645+Jepos@users.noreply.github.com>
2025-07-08 02:45:25 -0700

    product responsive fixed. Products images added

--- ymac/src/data/products.ts
+++ ymac/src/data/products.ts
@@ -1,20 +1,20 @@
 import type { Product } from "../types/product";
-
+ 
 const products:Product[] = [
     {
       id:1,
-      name: "340 MALLA NEGRO GRIS",
+      name: "340 MALLA NEGRA NEGRA",
       brand: "Caribu",
       description: [
-        "Tallas Del 25 Al 30",
+        "Tallas Del 22 Al 26",
         "Casco De Poliamida",
         "Dieléctrico",
         "Suela De Hule/Phylon"
       ],
       images: [
-        "images/botas1.jpg",
-        "images/botas2.png",
-        "images/botas1.jpg"
+        "images/products/1/default.png",
+        "images/products/1/2.png",
+        "images/products/1/3.png",
       ]
     },
 
@@ -29,96 +29,156 @@ const products:Product[] = [
         "Suela De Hule/Phylon"
       ],
       images: [
-        "images/botas2.png",
-        "images/botas2.png",
-        "images/botas2.png"
+        "images/products/2/default.png",
+        "images/products/2/2.png",
+        "images/products/2/3.png",
       ]
     },
 
     {
       id:3,
-      name: "340 MALLA NEGRO GRIS",
-      brand: "Caribu",
+      name: "BOTA DE SEGURIDAD PRO",
+      brand: "Duty Gear",
       description: [
-        "Tallas Del 25 Al 30",
-        "Casco De Poliamida",
-        "Dieléctrico",
-        "Suela De Hule/Phylon"
+        "Resistencia clásica con inyección directa y piel premium"
       ],
       images: [
-        "images/botas1.jpg",
-        "images/botas1.jpg",
-        "images/botas1.jpg"
+        "images/products/3/default.png",
+        "images/products/3/2.png",
+        "images/products/3/3.png",
       ]
     },
 
     {
-      id:3,
-      name: "340 MALLA NEGRO GRIS",
-      brand: "Caribu",
+      id:4,
+      name: "BOTA DE SEGURIDAD",
+      brand: "Romanos",
       description: [
-        "Tallas Del 25 Al 30",
+        "Tallas Del 22 Al 26",
         "Casco De Poliamida",
         "Dieléctrico",
         "Suela De Hule/Phylon"
       ],
       images: [
-        "images/botas1.jpg",
-        "images/botas1.jpg",
-        "images/botas1.jpg"
+        "images/products/4/default.png",
+        "images/products/4/2.png",
+        "images/products/4/3.png",
       ]
     },
 
     {
-      id:3,
-      name: "340 MALLA NEGRO GRIS",
+      id:5,
+      name: "954 CRAZY CAFÉ",
       brand: "Caribu",
       description: [
         "Tallas Del 25 Al 30",
         "Casco De Poliamida",
-        "Dieléctrico",
-        "Suela De Hule/Phylon"
+        "Piel 100% vacuno",
+        
       ],
       images: [
-        "images/botas1.jpg",
-        "images/botas1.jpg",
-        "images/botas1.jpg"
+        "images/products/5/default.png",
+        "images/products/5/2.png",
+        "images/products/5/3.png",
       ]
     },
 
     {
-      id:3,
-      name: "340 MALLA NEGRO GRIS",
+      id:6,
+      name: "954 HÚNGARO NEGRO",
       brand: "Caribu",
       description: [
         "Tallas Del 25 Al 30",
         "Casco De Poliamida",
-        "Dieléctrico",
-        "Suela De Hule/Phylon"
+        "Piel 100% vacuno",
+        
       ],
       images: [
-        "images/botas1.jpg",
-        "images/botas1.jpg",
-        "images/botas1.jpg"
+        "images/products/6/default.png",
+        "images/products/6/2.png",
+        "images/products/6/3.png",
       ]
     },
 
     {
-      id:3,
-      name: "340 MALLA NEGRO GRIS",
+      id:7,
+      name: "325 MALLA NEGRA AZUL",
       brand: "Caribu",
       description: [
-        "Tallas Del 25 Al 30",
-        "Casco De Poliamida",
-        "Dieléctrico",
+        "Tallas Del 22 Al 26",
+        "Casco De Poliamida",        
         "Suela De Hule/Phylon"
       ],
       images: [
-        "images/botas1.jpg",
-        "images/botas1.jpg",
-        "images/botas1.jpg"
+        "images/products/7/default.png",
+        "images/products/7/2.png",
+        "images/products/7/3.png",
       ]
-    }
+    },
+
+    {
+      id:8,
+      name: "325 MALLA NEGRA AMARILLA",
+      brand: "Caribu",
+      description: [
+        "Tallas Del 25 Al 30",
+        "Casco De Poliamida",        
+        "Suela De Hule/Phylon"
+      ],
+      images: [
+        "images/products/8/default.png",
+        "images/products/8/2.png",
+        "images/products/8/3.png",
+      ]
+    },
+
+    {
+      id:9,
+      name: "325 MALLA NEGRO NEGRO",
+      brand: "Caribu",
+      description: [
+        "Tallas Del 25 Al 30",
+        "Casco De Poliamida",        
+        "Suela De Hule/Phylon"
+      ],
+      images: [
+        "images/products/9/default.png",
+        "images/products/9/2.png",
+        "images/products/9/3.png",
+      ]
+    },
+
+    {
+      id:10,
+      name: "325 SINTÉTICO NEGRO GRIS",
+      brand: "Caribu",
+      description: [
+        "Tallas Del 25 Al 30",
+        "Casco De Poliamida",        
+        "Suela De Hule/Phylon"
+      ],
+      images: [
+        "images/products/10/default.png",
+        "images/products/10/2.png",
+        "images/products/10/3.png",
+      ]
+    },
+
+    {
+      id:11,
+      name: "325 MALLA AZUL GRIS",
+      brand: "Caribu",
+      description: [
+        "Tallas Del 25 Al 30",
+        "Casco De Poliamida",        
+        "Suela De Hule/Phylon"
+      ],
+      images: [
+        "images/products/11/default.png",
+        "images/products/1/2.png",
+        "images/products/1/3.png",
+      ]
+    },
 ]
 
 
